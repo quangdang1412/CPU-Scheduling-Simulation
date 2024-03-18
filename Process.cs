c0409725b342e8f06f594694ffb0bee1eafd21fe
quangdang1412 <111364708+quangdang1412@users.noreply.github.com>
2024-03-18 21:48:26 +0700

    Add files via upload

---
+++ Process.cs
@@ -0,0 +1,14 @@
+﻿namespace HDH_1
+{
+	public class Process
+	{
+		public int BurstTime { get; set; }
+		public int ArrivalTime { get; set; }
+		public int ProcessID { get; set; }
+		public double WaitingTime { get; set; }
+		public int RemainingTime { get; set; }
+		public int Priority { get; set; }
+		public int StartTime { get;  set; }
+		public int EndTime { get;  set; }
+	}
+}
