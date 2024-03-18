493771bf695de4f9bc93a32e19d386ee474b58e4
quangdang1412 <111364708+quangdang1412@users.noreply.github.com>
2024-03-18 21:50:32 +0700

    Add files via upload

--- HDH_1/Process.cs
+++ HDH_1/Process.cs
@@ -6,5 +6,9 @@
 		public int ArrivalTime { get; set; }
 		public int ProcessID { get; set; }
 		public double WaitingTime { get; set; }
+		public int RemainingTime { get; set; }
+		public int Priority { get; set; }
+		public int StartTime { get;  set; }
+		public int EndTime { get;  set; }
 	}
 }
